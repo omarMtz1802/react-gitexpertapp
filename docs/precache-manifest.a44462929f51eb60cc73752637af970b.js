7eb43f44012fd28e92f47ce43ad5acbbefd586fd
Oscar Martinez <oscar.martineza@elektra.com.mx>
2020-10-20 11:56:50 -0500

    docs add

---
+++ docs/precache-manifest.a44462929f51eb60cc73752637af970b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e3f22cde390bbbba788c2ca2cd5860d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c163625e5d46de462601",
+    "url": "/static/css/main.f9d1917a.chunk.css"
+  },
+  {
+    "revision": "395d653a30119579205e",
+    "url": "/static/js/2.7fbf3197.chunk.js"
+  },
+  {
+    "revision": "370b69322d3c8e1517a9e342e47a2dfb",
+    "url": "/static/js/2.7fbf3197.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c163625e5d46de462601",
+    "url": "/static/js/main.a135a11b.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
